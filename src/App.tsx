1366b619a5643bc450741a97b71d362b06c046df
Alvaro Iporre <alvaroiporremartinez@gmail.com>
2023-12-20 11:13:18 -0400

    Add Input component ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,13 @@
 import styled from 'styled-components';
 import GlobalStyles from './styles/GlobalStyles.ts';
 import Button from './ui/Button.tsx';
+import Input from './ui/Input.tsx';
 
 const H1 = styled.h1`
   font-size: 30px;
   font-weight: 600;
   background-color: yellow;
 `;
-
-const Input = styled.input`
-  border: 1px solid var(--color-grey-300);
-  background-color: var(--color-grey-0);
-  border-radius: var(--border-radius-sm);
-  box-shadow: var(--shadow-sm);
-  padding: 0.8rem 1.2rem;
-`;
-
 
 function App() {
 
