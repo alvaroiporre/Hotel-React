6d20b081b6bb8abd85ff655f961a6404eac5990b
Alvaro Iporre <alvaroiporremartinez@gmail.com>
2024-01-18 10:57:03 -0400

    Add Heading styled component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,15 @@
-import styled from 'styled-components';
 import GlobalStyles from './styles/GlobalStyles.ts';
 import Button from './ui/Button.tsx';
+import Heading from './ui/Heading.tsx';
 import Input from './ui/Input.tsx';
-
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-  background-color: yellow;
-`;
 
 function App() {
 
   return (
     <div>
       <GlobalStyles />
-      <H1>The Wild Oasis</H1>
-      <Button>Check in</Button>
+      <Heading>The Wild Oasis</Heading>
+      <Button onClick={() => alert('asdfa')}>Check in</Button>
       <Input type="number" placeholder="Guess the number"/>
     </div>
   );
