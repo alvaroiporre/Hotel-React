6460146fdf64fc46da845e120e321c3cdd2586e4
Alvaro Iporre <alvaroiporremartinez@gmail.com>
2024-01-18 21:20:35 -0400

    Add Users component page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from './pages/Dashboard'
 import Bookings from "./pages/Bookings";
 import Cabins from "./pages/Cabins";
+import Users from "./pages/Users";
 const App = () => {
   return (
     <BrowserRouter>
@@ -9,6 +10,7 @@ const App = () => {
         <Route path='dashboard' element={<Dashboard />}/>
         <Route path='bookings' element={<Bookings />}/>
         <Route path='cabins' element={<Cabins />}/>
+        <Route path='users' element={<Users />}/>
       </Routes>
     </BrowserRouter>
   );
