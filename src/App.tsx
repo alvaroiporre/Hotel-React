9dbe3a0b4c2ea9f1d79411422188a26637bc4cdc
Alvaro Iporre <alvaroiporremartinez@gmail.com>
2024-01-18 21:16:11 -0400

    Add Dashboard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,13 @@
-import GlobalStyles from './styles/GlobalStyles.ts';
-import Button from './ui/Button.tsx';
-import Heading from './ui/Heading.tsx';
-import Input from './ui/Input.tsx';
-import Row from './ui/Row.tsx';
-
-function App() {
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Dashboard from './pages/Dashboard'
+const App = () => {
   return (
-    <div>
-      <GlobalStyles />
-      <Row type='vertical'>
-        <Heading as='h1'>The Wild Oasis</Heading>
-        <Row type='horizontal'>
-          <Heading as='h2'>Check in and Out</Heading>
-          <div>
-            <Button variation='primary' size='medium' onClick={() => alert('asdfa')}>Check in</Button>
-            <Input type="number" placeholder="Guess the number" />
-          </div>
-        </Row>
-        <Row type='vertical'>
-
-          <Heading as='h3'> Say hello</Heading>
-          <form action="">
-
-            <Button variation='secondary' size='small' onClick={() => alert('asdfa')}>Check in</Button>
-            <Input type="number" placeholder="Guess the number" />
-          </form>
-        </Row>
-      </Row>
-    </div >
+    <BrowserRouter>
+      <Routes>
+        <Route path='dashboard' element={<Dashboard />}/>
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
-export default App;
+export default App; 
