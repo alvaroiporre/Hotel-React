9bce87c952d67ee0406a7ae6ed651d9d2d54b347
Alvaro Iporre <alvaroiporremartinez@gmail.com>
2024-01-18 21:17:35 -0400

    Add Booking component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from './pages/Dashboard'
+import Bookings from "./pages/Bookings";
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='dashboard' element={<Dashboard />}/>
+        <Route path='bookings' element={<Bookings />}/>
       </Routes>
     </BrowserRouter>
   );
