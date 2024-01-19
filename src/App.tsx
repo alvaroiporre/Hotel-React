45d74a02b358254b464bfa7f1315bd356b3e9ae6
Alvaro Iporre <alvaroiporremartinez@gmail.com>
2024-01-18 21:19:02 -0400

    Add Cabins component page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from './pages/Dashboard'
 import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='dashboard' element={<Dashboard />}/>
         <Route path='bookings' element={<Bookings />}/>
+        <Route path='cabins' element={<Cabins />}/>
       </Routes>
     </BrowserRouter>
   );
