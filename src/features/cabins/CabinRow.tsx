80f3d997611c8556c0f293e227d018223bf48d03
Alvaro Iporre <alvaroiporremartinez@gmail.com>
2024-01-21 19:16:07 -0400

    Add CabinRow

--- src/features/cabins/CabinRow.tsx
+++ src/features/cabins/CabinRow.tsx
@@ -38,3 +38,11 @@ const Discount = styled.div`
   font-weight: 500;
   color: var(--color-green-700);
 `;
+
+const CabinRow = () => {
+  return (
+    <div>CabinRow</div>
+  );
+};
+
+export default CabinRow;
