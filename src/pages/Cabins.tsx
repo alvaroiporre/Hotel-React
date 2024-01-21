bff44617d9fd4e027a2207f83a26f8fc6fc789b6
Alvaro Iporre <alvaroiporremartinez@gmail.com>
2024-01-21 18:45:28 -0400

    Add cabinTable component

--- src/pages/Cabins.tsx
+++ src/pages/Cabins.tsx
@@ -1,20 +1,18 @@
-import { useEffect } from "react";
+import CabinTable from "../features/cabins/CabinTable";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import { getCabins } from "../services/apiCabins";
 
 const Cabins = () => {
 
-  useEffect(() => {
-    getCabins().then((data) => console.log(data));
-  }, [])
-
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-      <img src="https://qubuskmtzumgefcphksh.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg" alt="test" />
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+      </Row>
+      <Row>
+        <CabinTable></CabinTable>
+      </Row>
+    </>
   );
 };
 
