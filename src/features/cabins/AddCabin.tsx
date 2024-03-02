f145adda3f10862f24e3331a64aecde192beb226
Alvaro Iporre <alvaroiporremartinez@gmail.com>
2024-03-02 19:59:02 -0400

    Add AddCabin component

---
+++ src/features/cabins/AddCabin.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import Button from "../../ui/Button";
+import Modal from "../../ui/Modal";
+import CreateCabinForm from "./CreateCabinForm";
+
+const AddCabin = () => {
+  const [isOpenModal, setIsOpenModal] = useState<boolean>(false);
+
+  return (
+    <div><Button onClick={() => setIsOpenModal((show) => !show)}>Add new cabin</Button>
+      {
+        isOpenModal && <Modal><CreateCabinForm /></Modal>
+      }</div>
+  );
+};
+
+export default AddCabin;
+
